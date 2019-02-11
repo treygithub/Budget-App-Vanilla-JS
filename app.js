7bb10b595209a488df0883454c453220b8865e44
Robert Hunnicutt <robhunn74@gmail.com>
2019-02-10 22:06:51 -0600

    big push

--- app.js
+++ app.js
@@ -1,7 +1,57 @@
 //budget controller
 let budgetController = ( ()=>{
    
-    //do something
+    let Expense = function(id,description,value){
+      this.id = id,
+      this.description = description,
+      this.value = value
+    }
+    let Income = function(id,description,value){
+      this.id = id,
+      this.description = description,
+      this.value = value
+    }
+
+   const data = {
+     allItems:{
+       exp:[],
+       inc:[]
+     },
+     totals:{
+       exp:0,
+       inc:0
+     }
+   }
+
+   return{
+     addItem:((type,des,val)=>{
+
+       var newItem, ID;
+
+       //Creat new ID
+       if(data.allItems[type].length > 0 ){
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+       } else{
+         ID = 0
+       }
+
+       //Creat new item/object based on inc or exp value aka type
+        if(type === 'exp'){
+          newItem = new Expense(ID,des,val);
+        } else if(type === 'inc'){
+          newItem = new Income(ID,des,val);
+        }
+
+        //push new item into data object
+        data.allItems[type].push(newItem);
+
+        //return new item
+        return newItem;
+     }),
+     testing: function(){
+       console.log('data', data)
+     }
+   }
 
 })();
 
@@ -51,11 +101,13 @@ let controller = ( (budgetCtrl, UICtrl)=>{
 
   var ctrlAddItem = ()=>{
 
+    let input, newItem;
+
     //get field input data
-    let input = UICtrl.getInput();
-    console.log('input', input);
+    input = UICtrl.getInput();
 
     //add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     //add the item to the ui
 
